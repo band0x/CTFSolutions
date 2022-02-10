0f4fc6c94a669c5d8f305e2fb9d786eddc1a7a2a
band0x <98318897+band0x@users.noreply.github.com>
2022-02-10 11:30:21 +1100

    Create MayTheForceBeWithYou.sol

---
+++ MayTheForceBeWithYou.sol
@@ -0,0 +1,3 @@
+//Solution and writeup for https://www.defihack.xyz/level/0xB761C26d6Ade3Da1c6a4313C785b76B90cae702e
+
+//Start with importing the contract code into Visual Studio Code, analysing with Solidity VIsual Developer to get a high level view of functions and interactions.
